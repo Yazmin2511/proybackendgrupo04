807eb4bd1f3102e9224bc2195212d9c2f48841ca
Yazmin2511 <yazmin2511@gmail.com>
2022-06-23 18:55:40 -0300

    Creacion de model de Recurso

--- Backend/controllers/reunion.controller.js
+++ Backend/controllers/reunion.controller.js
@@ -1,4 +1,60 @@
 const Reunion = require('../models/reunion');
 const reunionCtrl = {}
 
+reunionCtrl.createReunion = async (req,res) => {
+    var reunion = new Reunion(req,body);
+    try{
+        await reunion.save();
+        res.json({
+            'status':'1',
+            'msg':'Reunion guardada.'
+        })
+    }catch(error){
+        res.status(400).json({
+            'status':'0',
+            'msg':'Error procesando operacion.'
+        })
+    }
+}
+
+reunionCtrl.getReuniones = async (req,res) => {
+    const reuniones = await Reunion.find().populate('reunion').exec();
+    res.json(reuniones);
+}
+
+reunionCtrl.deleteReunion  = async (req,res) => {
+    try{
+        await Reunion.deleteOne({_id: req.params.id});
+        res.json({
+            'status':'1',
+            'msg':'Pasaje deleted.'
+        })
+    }catch(error){
+        res.status(400).json({
+            'status':'0',
+            'msg':'Error procesando la operacion.'
+        })
+    }
+}
+
+reunionCtrl.editReunion = async (req,res) => {
+    const vreunion = new Reunion(req.body);
+    try {
+        await Reunion.updateOne({_id: req.body}, vreunion)
+        res.json({
+            'status':'1',
+            'msg':'Reunion updated.'
+        })
+    }catch(error){
+        res.status(400).json({
+            'status':'0',
+            'msg':'Error procesando la operacion.'
+        })
+    }
+}
+
+// get reunion x != participante
+
+// reunion por dia , mes o
+
 module.exports = reunionCtrl;
